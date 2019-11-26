7104a27e8fde4c760b3d623421df3dcc23e88f09
Rafał Żygadło <rafal@maxkod.pl>
2019-11-26 13:19:54 +0100

    Część 5 – Pierwszy plik w projekcie. Klasa App.

---
+++ src/app.cpp
@@ -0,0 +1,17 @@
+#include "app.h"
+#include "frame.h"
+
+
+IMPLEMENT_APP(App)
+
+App::App()
+{
+}
+
+
+bool App::OnInit()
+{	
+	
+	return true;
+}
+
---
+++ src/app.h
@@ -0,0 +1,18 @@
+#ifndef __APP
+#define __APP
+#include <wx/wx.h>
+#include <wx/frame.h>
+
+class App: public wxApp
+{
+
+  public:
+	
+	App();
+    virtual bool OnInit();
+	
+	
+    
+};
+
+#endif 
